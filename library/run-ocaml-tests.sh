11e8c0d1e19d41d34ddff7aea024c742ae3a3054
Yao Li <hnkfliyao@gmail.com>
2018-06-07 10:10:36 -0700

    Edit run-ocaml-tests.sh.
    Symbolic links sometimes do not work properly on Cygwin. This fixes a compilation problem that would happen on Cygwin, without affecting other OSes.

--- library/run-ocaml-tests.sh
+++ library/run-ocaml-tests.sh
@@ -4,14 +4,11 @@ dir=$1
 file=list
 cd $dir
 
-rm -f ocaml-lib
-ln -s ../../ocaml-lib .
-
 for file in *Auxiliary.ml
 do
   echo $file
   file_nat=${file%.ml}.native
-  ocamlfind ocamlc -package zarith -linkpkg -o ${file_nat} -I ocaml-lib/_build_zarith extract.cma ${file}
+  ocamlfind ocamlc -package zarith -linkpkg -o ${file_nat} -I ../../ocaml-lib/_build_zarith extract.cma ${file}
 done
 
 for file in *.native
